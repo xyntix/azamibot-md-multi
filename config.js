c8c2f3bc9ac8a20cdadfa45ff5e38a39114ae58b
xyntix <61931770+xyntix@users.noreply.github.com>
2022-10-18 21:23:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-	['6285282250251'],
-	['79309965233'],
-	['6282337245566', 'Syahrul A.S', true]
+	['16028253490'],
+	['16028253490'],
+	['16028253490', 'Reeya', true]
 	// [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
